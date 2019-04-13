e8171a1aa4b7de23dfb8fe9395729911d72a613a
Micro <microa4@gmail.com>
2019-04-13 21:29:25 +0200

    added button reset on each counter

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -2,14 +2,14 @@ import './Counter.css';
 
 
 import React, { Component } from 'react';
- 
+
 // let fontSize = 17;
 
 //Aqui se crea la vista para mostrar
 class Counter extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       fontSize: 17,
       backgroundColor: '',
@@ -21,28 +21,30 @@ class Counter extends Component {
   suma() {
     console.log(this.state.num);
 
-    this.setState({ num: this.state.num += this.props.step || 1,   fontSize: this.state.fontSize += this.props.step || 1 });
+    this.setState({ num: this.state.num += this.props.step || 1, fontSize: this.state.fontSize += this.props.step || 1 });
     // fontSize=fontSize + 1 ;
     // console.log(this.state.num);
 
   }
 
-
+  reset() {
+    this.setState({ num: this.state.num = this.props.startFrom || 0, fontSize: this.state.fontSize = 17 });
+  }
 
 
 
   //devolver vista
   render() {
     console.log();
-    let style={fontSize: this.state.fontSize + "px", };
-    if(this.state.num === 0){
-      style={fontSize: this.state.fontSize + "px", backgroundColor: this.state.backgroundColor = 'red'};
+    let style = { fontSize: this.state.fontSize + "px", };
+    if (this.state.num === 0) {
+      style = { fontSize: this.state.fontSize + "px", backgroundColor: this.state.backgroundColor = 'red' };
     }
-   
+
     return (
       <span className="Counter">
         <button style={style} onClick={this.suma.bind(this)}>{this.state.num}</button>
-
+        <buton className="reset" onClick={this.reset.bind(this)}>R</buton>
 
       </span>
     );
