c108d7c4056615a434d88c130ce9458b6f113f59
Micro <microa4@gmail.com>
2019-04-13 20:52:27 +0200

    funcionando crecimiento de los counter 100%

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -3,24 +3,26 @@ import './Counter.css';
 
 import React, { Component } from 'react';
  
-let fontSize = 17;
+// let fontSize = 17;
 
 //Aqui se crea la vista para mostrar
 class Counter extends Component {
   constructor(props) {
     super(props);
+    
     this.state = {
+      fontSize: 17,
       num: props.startFrom || 0
 
     }
   }
 
   suma() {
+    console.log(this.state.fontSize);
 
-    this.setState({ num: this.state.num += this.props.step || 1 });
-    fontSize=fontSize + 1 ;
+    this.setState({ num: this.state.num += this.props.step || 1,   fontSize: this.state.fontSize += 1  });
+    // fontSize=fontSize + 1 ;
     // console.log(this.state.num);
-    console.log(fontSize);
 
   }
 
@@ -30,7 +32,7 @@ class Counter extends Component {
 
   //devolver vista
   render() {
-   let style={fontSize};
+   let style={fontSize: this.state.fontSize + "px"};
     return (
       <span className="Counter">
         <button style={style} onClick={this.suma.bind(this)}>{this.state.num}</button>
